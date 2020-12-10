62bebe1f8c32a7d0625c37531dc2dc4bc55e38e8
Devesteves <Esteveshxc@outlook.com>
2020-12-10 17:40:21 -0300

    teste

---
+++ teste.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef struct Fila{
+    int ini;
+    int meio;
+    int fim;
+    int fila;
+}; fila; 
+
+main(void)
+{
+    int algo;
+    printf("%d",fila.ini);
+    printf("%d",algo);
+    return 0;
+}
